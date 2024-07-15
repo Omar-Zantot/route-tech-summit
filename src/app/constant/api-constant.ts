1ea373fc2d9c654c0d08b77d329598c0b8ab34e6
Omar-Zantout <omar82199@gmail.com>
2024-07-16 00:05:49 +0300

    Alter api baseUrl

--- src/app/constant/api-constant.ts
+++ src/app/constant/api-constant.ts
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://localhost:3000';
+export const baseUrl = 'https://api-snowy-six-76.vercel.app';
 export const customersUrl = baseUrl + '/customers';
 export const transactionsUrl = baseUrl + '/transactions';
 
